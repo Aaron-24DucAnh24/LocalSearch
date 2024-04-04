294257cde66bc5751509eb6c0d394b687966d2d0
= <lccongthanh1102@gmail.com>
2024-04-04 22:39:02 +0700

    fix: ImageTraversal#next_pos and RestartHillClimbing#search

--- LocalSearch/problem.py
+++ LocalSearch/problem.py
@@ -1,26 +1,25 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
 class ImageTraversal:
     ACTIONS = np.array(["L", "R", "U", "D", "LU", "LD", "RU", "RD"])
+
     def __init__(self, image_uri) -> None:
         self.space = self.load_image(image_uri)
         self.H, self.W = self.space.shape
-            
+
     def next(self, position):
         """
         Return list of children
         """
         for _, new_pos in self.actions(position):
             yield new_pos
-    
+
     def objective_value(self, x, y):
-        # print(f'The space as position {x} and {y}')
-        # print(f'is: {self.space[x,y]}')
-        print(f'space: {self.space.shape}')
         return self.space[x, y]
-    
+
     def actions(self, position):
         """
         @return list of available actions
@@ -31,52 +30,59 @@ class ImageTraversal:
                 yield action, next_pos
 
     def next_pos(self, position, action):
-        if "L" in action: position.x -= 1
-        if "R" in action: position.x += 1
-        if "U" in action: position.y += 1
-        if "D" in action: position.y -= 1
-        return position
-    
+        new_position = deepcopy(position)
+        if "L" in action: new_position.x -= 1
+        if "R" in action: new_position.x += 1
+        if "U" in action: new_position.y += 1
+        if "D" in action: new_position.y -= 1
+        new_position.z = self.objective_value(new_position.x, new_position.y)
+        return new_position
+
     def load_image(self, filename):
         # remove colors => z ranges from 0 to 255
-        img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE) 
+        img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
         # remove colors => z ranges from 0 to 
-        img = cv2.resize(img, (0, 0), fx=0.25, fy=0.25) 
+        img = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
         # create state space landscape
-        img = cv2.GaussianBlur(img, (5, 5), 0) 
-        return img 
-    
+        img = cv2.GaussianBlur(img, (5, 5), 0)
+        return img
+
     def visualize(position):
         X, Y, Z = position
         X, Y = np.meshgrid(X, Y)
-        fig = plt.figure(figsize=(8,6))
+        fig = plt.figure(figsize=(8, 6))
         ax = plt.axes(projection='3d')
         # draw state space (surface)
         ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
         # draw a polyline on the surface
         ax.plot(range(0, 50), range(0, 50), Z[range(0, 50), range(0, 50)], 'r-', zorder=3, linewidth=0.5)
         plt.show()
-    
+
     class Position:
         """
         Utility class to work with the problem state
         """
+
         def __init__(self, x, y, z):
             self.x = x
             self.y = y
             self.z = z
-            if x < 0 or y < 0: raise Exception(f"{x if x < 0 else y} must be non-negative") 
+            if x < 0 or y < 0: raise Exception(f"{x if x < 0 else y} must be non-negative")
             if not (0 <= z <= 255): raise Exception(str(z) + " must between 0 and 255 (inclusive)")
-        
+
         def __lt__(self, other):
             return self.z > other.z
-        
+
         @staticmethod
         def to_tuple3(position):
-            return (position.x, position.y, position.z)
-        
+            return position.x, position.y, position.z
+
         @staticmethod
         def validate(position) -> bool:
             if position.x < 0: return False
             if position.y < 0: return False
             if position.z > 255 or position.z < 0: return False
+            return True
+
+        def __str__(self):
+            return f"({self.x} {self.y} {self.z})"
--- LocalSearch/search.py
+++ LocalSearch/search.py
@@ -1,65 +1,69 @@
+import logging
+
 from problem import ImageTraversal
 from abc import ABC, abstractmethod
 import numpy as np
-import heapq
 from functools import reduce
+
 
 class LocalSearchStrategy(ABC):
     @abstractmethod
     def search(self, problem: ImageTraversal) -> list[ImageTraversal.Position]: pass
-    
-    
+
+
 class RandomRestartHillClimbing(LocalSearchStrategy):
     def __init__(self, num_trial: int):
         self.num_trial = num_trial
-        
+
     def search(self, problem: ImageTraversal) -> list[ImageTraversal.Position]:
         return self.random_restart_hill_climbing(problem, self.num_trial)
-    
-    @staticmethod   
+
+    @staticmethod
     def random_restart_hill_climbing(problem, num_trial):
         results = []
         for initial_state in RandomRestartHillClimbing.generate_random_state(problem, num_trial):
-            path = RandomRestartHillClimbing.__hill_climbing(problem, initial_state)
-            results.append(path)
-        return reduce(lambda best, candidate: candidate if candidate[:-1][3] > best[:-1][3] else best, results)
-    
-    def __hill_climbing(problem, init):
+            results.append(RandomRestartHillClimbing.__hill_climbing(problem, initial_state))
+        print("Candidate paths")
+        for result in results: print(result)
+        return reduce(lambda best, candidate: candidate if candidate[-1][2] > best[-1][2] else best, results)
+
+    @staticmethod
+    def __hill_climbing(problem: ImageTraversal, init):
         path = [init]
         while True:
             curr = path[-1]
-            print(f'this is problem: {problem}')
-            print(f'this is curr: {curr}')
             candidate = max(problem.next(curr), key=lambda position: position.z)
             if candidate.z > curr.z:
                 path.append(candidate)
-            else: break
+            else:
+                break
         return list(map(lambda position: ImageTraversal.Position.to_tuple3(position), path))
-    
-    def generate_random_state(problem, num_trial):
+
+    @staticmethod
+    def generate_random_state(problem, num_trial=1):
         for _ in range(0, num_trial):
             y = np.random.randint(problem.W)
             x = np.random.randint(problem.H)
             yield ImageTraversal.Position(x, y, problem.objective_value(x, y))
-        
-   
+
+
 class SimulatedAnnealing(LocalSearchStrategy):
     def __init__(self, schedule):
         self.schedule = schedule
-    
+
     def search(self, problem: ImageTraversal) -> list[ImageTraversal.Position]:
         return self.simulated_annealing_search(problem, self.schedule)
-    
+
     @staticmethod
     def simulated_annealing_search(problem, schedule): pass
-    
+
+
 class LocalBeamSearch(LocalSearchStrategy):
-    def __init__(self, k = 1):
+    def __init__(self, k=1):
         self.beam_width = k
-    
+
     def search(self, problem: ImageTraversal) -> list[ImageTraversal.Position]:
         return self.local_beam_search(problem, self.beam_width)
-    
+
     @staticmethod
-    def local_beam_search(problem, schedule): pass 
-            
+    def local_beam_search(problem, schedule): pass
