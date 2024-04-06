cde9794a2889524ab67acca6ae7a1b05d1055ba2
vdtquyen <vdtq12@gmail.com>
2024-04-06 19:10:18 +0700

    fix: test name

--- LocalSearch/test.py
+++ LocalSearch/test.py
@@ -4,7 +4,7 @@ from problem import ImageTraversal
 if __name__ == "__main__":
     inp = input("Alg: RandomRestartHillClimbing. Please input number of trial: ")
     s = RandomRestartHillClimbing(int(inp))
-    p = ImageTraversal('camera.jpg')
+    p = ImageTraversal('test3_shoulder.jpg')
     path = s.search(p)
     print(f"path found: {path}")
     p.draw_path(path)
